25b8be556f445bc038fd28f4edc0536fd83bda16
Andy Meneely <andy.meneely@gmail.com>
2023-03-10 00:27:09 -0500

    run archeogit on kernel

--- migrations/2023_03_09.archeogit-kernel-vccs.rb
+++ migrations/2023_03_09.archeogit-kernel-vccs.rb
@@ -5,30 +5,29 @@ require 'set'
 # MIGRATION STATUS: Done! For now at least. This could be rerun.
 # raise 'Migration already performed.' # Don't run this migration. Kept for posterity
 
-Dir['cves/kernel/CVE\-2020*.yml'].each do |yml_file|
-  yml = YAML.load(File.open(yml_file, 'r').read)
-
+Dir['cves/kernel/CVE*.yml'].each do |yml_file|
+  yml = YAML.load(File.open(yml_file, 'r').read,
+                  permitted_classes: [Date, Symbol])
   vcc_set = Set.new
   begin
-  yml['fixes'].each do |fix|
-    sha = fix['commit']
-    unless sha.to_s.empty?
-      Dir.chdir '/Users/andy/code/archeogit' do
-        str = `archeogit blame --csv /Users/andy/code/linux #{sha} `
-        CSV.parse(str, headers: true, return_headers: false).each do |row|
-          vcc_set << row[2]
+    yml['fixes'].each do |fix|
+      sha = fix['commit']
+      unless sha.to_s.empty?
+        Dir.chdir '/Users/andy/code/archeogit' do
+          str = `archeogit blame --csv /Users/andy/code/linux #{sha} `
+          CSV.parse(str, headers: true, return_headers: false).each do |row|
+            vcc_set << row[2]
+          end
         end
       end
     end
-  end
-  rescue
+  rescue => e
     require 'byebug'; byebug
   end
 
   yml['vccs'] = vcc_set.map do |vcc|
-    {'commit' => vcc, note: 'Discovered automatically by archeogit.' }
+    {'commit' => vcc, 'note' => 'Discovered automatically by archeogit.' }
   end
-  # require 'byebug'; byebug
 
   # Generate the new YML, clean it up, write it out.
   File.open(yml_file, "w+") do |file|
